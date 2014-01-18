7d0fea912079b682745ed32e2503f770d7898e55
Jymn_Chen <guesn10000@126.com>
2014-01-19 01:38:01 +0800

    Initial commit
    Build SCNUPaper on GitHub

---
+++ SCNUPaperTests/______Tests.m
@@ -0,0 +1,34 @@
+//
+//  ______Tests.m
+//  论文批阅系统Tests
+//
+//  Created by Jymn_Chen on 13-11-17.
+//  Copyright (c) 2013年 Jymn_Chen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ______Tests : XCTestCase
+
+@end
+
+@implementation ______Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
